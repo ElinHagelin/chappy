85f2c4e9d3457cf2ac02be0fbb88d6d97c7e1f73
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-06-14 09:51:23 +0200

    added start script

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -69,35 +69,42 @@ const Chat = () => {
 
 	return (
 		<div className="chat-area">
-			<section className="heading">
-				Chattar i <span className="chat-name"> #{selectedChatName} </span>
-			</section>
-			<section className="history">
+			{selectedChatId ? (
+				<>
+					<section className="heading">
+						Chattar i <span className="chat-name"> #{selectedChatName} </span>
+					</section>
+					<section className="history">
 
-				{chatMessages ?
-					chatMessages.map(message => (
-						<section key={message.id} className={message.sender === userId ? 'align-right' : null}>
-							<p> {message.sender === 0 ? 'Anonym' : message.sender}:
-								{editMode === message.id ? (<input type="text"
-									onChange={(e) => setNewMessage(e.target.value)}
-									value={newMessage}></input>)
-									: message.message} </p>
-							<p> {message.time} </p>
-							{message.sender === userId && (
-								<>
-									<button onClick={() => handleEdit(message.id)}> Redigera </button>
-									<button onClick={() => handleDelete(message.id)}> Ta bort </button>
-								</>
-							)}
-						</section>
-					)
-					) : <p>No messages yet...</p>}
+						{chatMessages ?
+							chatMessages.map(message => (
+								<section key={message.id} className={message.sender === userId ? 'align-right' : null}>
+									<p> {message.sender === 0 ? 'Anonym' : message.sender}:
+										{editMode === message.id ? (<input type="text"
+											onChange={(e) => setNewMessage(e.target.value)}
+											value={newMessage}></input>)
+											: message.message} </p>
+									<p> {message.time} </p>
+									{message.sender === userId && (
+										<>
+											<button onClick={() => handleEdit(message.id)}> Redigera </button>
+											<button onClick={() => handleDelete(message.id)}> Ta bort </button>
+										</>
+									)}
+								</section>
+							)
+							) : <p>No messages yet...</p>}
 
-			</section>
-			<section>
-				<input type="text" placeholder="Ditt meddelande..." value={message} onChange={(e) => setMessage(e.target.value)} />
-				<button onClick={handleSubmit}> Skicka </button>
-			</section>
+					</section>
+					<section>
+						<input type="text" placeholder="Ditt meddelande..." value={message} onChange={(e) => setMessage(e.target.value)} />
+						<button onClick={handleSubmit}> Skicka </button>
+					</section>
+				</>
+			)
+				: <section className="heading">
+					Välj en kanal för att börja chatta
+				</section>}
 		</div>
 	)
 }
