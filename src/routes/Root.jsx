1203086a26c5fc0844dcb51de72b022212212101
Elin Hagelin <elinsofiahagelin@gmail.com>
2023-06-15 21:31:35 +0200

    fixed imports in Root.jsx

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Header from '../components/Header'
-import NavBar from '../components/navBar'
-import Chat from '../components/Chat'
-import { ssKey } from '../components/LoginForm'
+import Header from '../components/Header.jsx'
+import NavBar from '../components/NavBar.jsx'
+import Chat from '../components/Chat.jsx'
+import { ssKey } from '../components/LoginForm.jsx'
 import { useRecoilState } from 'recoil'
-import loggedInAtom from '../recoil/loggedInAtom'
-import userIdAtom from '../recoil/userIdAtom'
-import loggedInUserAtom from '../recoil/loggedInUserAtom'
-import { getUserFromJWT } from '../utils/ajax/ajaxUsers'
+import loggedInAtom from '../recoil/loggedInAtom.js'
+import userIdAtom from '../recoil/userIdAtom.js'
+import loggedInUserAtom from '../recoil/loggedInUserAtom.js'
+import { getUserFromJWT } from '../utils/ajax/ajaxUsers.js'
 
 function Root() {
 	const [isLoggedIn, setIsLoggedIn] = useRecoilState(loggedInAtom)
